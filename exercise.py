c18be1047a5eab5aab4e4fb9237054c8c63ef163
Luoadore <luoluomayday@gmail.com>
2018-03-09 21:54:23 +0800

    add JSON

--- exercise.py
+++ exercise.py
@@ -335,4 +335,22 @@ def Findfile(path, s):
 mypath = 'F:\codestore\hyperspectral_exp_orz\\tf_try\GANs'
 Findfile(mypath, 'cgan')"""
 
-"""exercise 24"""
+"""exercise 24
+import json
+
+class Student:
+ def __init__(self, name, age, score):
+  self.name = name
+  self.age = age
+  self.score = score
+
+def student2dict(std):
+ return{
+        'name': std.name,
+        'age': std.age,
+        'score': std.score
+ }
+s = Student('Bob', 20, 88)
+print(json.dumps(s, default = student2dict))"""
+
+"""exercise 25"""
