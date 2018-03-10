f219c87a99a26805d121c3c36d2bb7be63e7ce36
Luoadore <luoluomayday@gmail.com>
2018-03-10 22:31:49 +0800

    add process

--- exercise.py
+++ exercise.py
@@ -353,4 +353,52 @@ def student2dict(std):
 s = Student('Bob', 20, 88)
 print(json.dumps(s, default = student2dict))"""
 
-"""exercise 25"""
+"""exercise 25
+from multiprocessing import Process
+import os
+
+# 子进程要执行的程序
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target = run_proc, args = ('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
+
+# 进程间的通信
+from multiprocessing import Process, Queue
+import os, time, random
+
+# 写数据进程
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+# 读
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+if __name__ == '__main__':
+    # 父进程创建Queue，并传给各个子进程
+    q = Queue()
+    pw = Process(target = write, args = (q,))
+    pr = Process(target = read, args = (q,))
+    # 启动子进程pw， 写入：
+    pw.start()
+    # 启动子进程pr，读取：
+    pr.start()
+    # 等待pw结束
+    pw.join()
+    pr.terminate()"""
+
+"""exercise 26"""
