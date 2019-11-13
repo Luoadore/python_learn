11da06dc4b2627b6754c7e01d82cb17fc7bea697
luoadore <luoluomayday@gmail.com>
2019-11-13 23:11:15 +0800

    be better girl

--- leetcode.py
+++ leetcode.py
@@ -222,3 +222,242 @@ def f_s_c(A, B, C, D):
 
 # 797所有可能的路径
 # 有向无环图
+# Let N be the number of nodes in the graph. 
+# If we are at node N-1, the answer is just the path {N-1}. Otherwise, 
+# if we are at node node, the answer is {node} + {path from nei to N-1} for each neighbor nei of node. 
+# This is a natural setting to use a recursion to form the answer.
+def allPathsSourceTarget(graph):
+	N = len(graph)
+
+	def solver(node):
+		if node == N - 1:
+			return [[N - 1]]
+		ans = []
+		for nei in graph[node]:
+			for path in solver(nei):
+				ans.append([node] + path)
+		return ans
+	return solver(0)
+
+# 763划分字母区间
+# 贪心，找到每个字母最后出现的位置
+# 维护partition的起点和终点
+# 时间复杂度O(n)
+def partitionLabels(S):
+	last = {c: i for i, c in enumerate(S)}
+	print(last)
+	j = anchor = 0
+	ans = []
+	for i, c in enumerate(S):
+		j = max(j, last[c])
+		if i == j:
+			ans.append(i - anchor + 1)
+			anchor = i + 1
+	return ans
+
+#print(partitionLabels('ababcabde'))
+
+# 56合并区间
+# 
+def merge(intervals):
+	inter = [x[i] for x in intervals for i in range(2)]
+	print(inter)
+	i = 1
+	while i < len(inter) - 1:
+		j = i + 1
+		if j < len(inter) and inter[i] >= inter[j]:
+			inter.pop(i)
+			inter.pop(j - 1)
+		i += 2
+	print(inter)
+	return [[inter[i], inter[j]] for i, j in zip(range(0, len(inter) - 1, 2), range(1, len(inter), 2))]
+
+
+print(merge([[1,3],[2,6],[8,10],[15,18]]))
+print(merge([[1,4],[4,5]]))
+
+# 13罗马数字转整数
+def romanToInt(s):
+    """
+    type s: str
+    rtype: int
+    """
+    roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    s_len = len(s)
+    integer, i, addend, flag = 0, 0, 0, True
+    while i < s_len:
+        if i + 1 < s_len:
+            print('nice:', i)
+            if s[i] == 'I' and (s[i + 1] == 'V' or s[i + 1] == 'X'):
+                addend = roman_dict[s[i + 1]] - roman_dict[s[i]]
+                i += 2
+                flag = False
+            elif s[i] == 'X' and (s[i + 1] == 'L' or s[i + 1] == 'C'):
+                addend = roman_dict[s[i + 1]] - roman_dict[s[i]]
+                i += 2
+                flag = False
+            elif s[i] == 'C' and (s[i + 1] == 'D' or s[i + 1] == 'M'):
+                addend = roman_dict[s[i + 1]] - roman_dict[s[i]]
+                i += 2
+                flag = False
+        if flag:
+            addend = roman_dict[s[i]]
+            i += 1
+        #print(addend)
+        integer += addend       
+    return integer
+
+print(romanToInt('MCMXCIV'))
+print(romanToInt('LVIII'))
+print(romanToInt('IX'))
+print(romanToInt('IV'))
+print(romanToInt('III'))
+
+# 42 接雨水
+# 分治：最大次大之间区域间雨水量
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        def raindrop(height):
+            rain = 0
+            if len(height) < 2:
+                return rain
+            h = min(height[0], height[-1])
+            rain = h * (len(height) - 2) - sum(height[1:-1])
+            return rain
+        rain = 0
+        l, r = 0, len(height) - 1
+        if l < r - 1: 
+            i1, i2 = height.index(max(height)), 0
+            minus = sys.maxsize
+            for j in range(len(height)):
+                if j != i1 and height[i1] - height[j] < minus:
+                    minus = height[i1] - height[j]
+                    i2 = j
+            l, r = min(i1, i2), max(i1, i2)
+            rain = raindrop(height[l:r + 1]) + self.trap(height[0:l + 1]) + self.trap(height[r:])
+        return rain
+
+# 84 柱状图中最大的矩形
+# 分治：最小值对应的面积，有序数列优化（注意时长）
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        def area(heights):
+            min_area = 0
+            if len(heights) >= 1:
+                min_heights = min(heights)
+                min_index = heights.index(min_heights)
+                min_area = min_heights * len(heights)
+                
+                max_tmp = 0
+                if heights == sorted(heights):
+                    for h in range(len(heights)):
+                        tmp = (len(heights) - h) * heights[h]
+                        if max_tmp < tmp:
+                            max_tmp = tmp
+                            
+                    return max_tmp
+                
+                if heights == sorted(heights)[::-1]:
+                    for h in range(len(heights)):
+                        tmp = (h + 1) * heights[h]
+                        if max_tmp < tmp:
+                            max_tmp = tmp
+                            
+                    return max_tmp
+                
+                return max(min_area, area(heights[:min_index]), area(heights[min_index+1:]))
+            else:
+                return min_area
+        
+        area_heights = area(heights)
+        return area_heights
+
+# 85 最大矩形
+# 先求每行连续1的个数，再按列算柱状图最大矩形
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        def get_bar(heights):
+            
+            if len(heights) == 0:
+                return []
+            
+            if heights[0] == "1":
+                flag = 1
+            else:
+                flag = 0
+            heights[0] = int(heights[0])
+            for i in range(1, len(heights)):
+                if heights[i] == "1":
+                    if flag == 1:
+                        heights[i] = heights[i-1] + 1
+                    else:
+                        heights[i] = 1
+                        flag = 1
+                else:
+                    heights[i] = 0
+                    flag = 0
+
+            return heights
+
+        def max_rectangle(heights):
+
+            def area(heights):
+                min_area = 0
+                if len(heights) >= 1:
+                    min_heights = min(heights)
+                    min_index = heights.index(min_heights)
+                    min_area = min_heights * len(heights)
+                    
+                    max_tmp = 0
+                    if heights == sorted(heights):
+                        for h in range(len(heights)):
+                            tmp = (len(heights) - h) * heights[h]
+                            if max_tmp < tmp:
+                                max_tmp = tmp
+                                
+                        return max_tmp
+                    
+                    if heights == sorted(heights)[::-1]:
+                        for h in range(len(heights)):
+                            tmp = (h + 1) * heights[h]
+                            if max_tmp < tmp:
+                                max_tmp = tmp
+                                
+                        return max_tmp
+                    
+                    return max(min_area, area(heights[:min_index]), area(heights[min_index+1:]))
+                else:
+                    return min_area
+        
+            return area(heights)
+        
+        if len(matrix) == 0:
+            return 0
+
+        max_area = 0
+
+        matrix_bar = []
+        for each in matrix:
+            each_bar = get_bar(each)
+            matrix_bar.append(each_bar)
+        print(matrix_bar)
+        
+        for i in range(len(matrix[0])):
+            each_bar = [matrix[j][i] for j in range(len(matrix))]
+            each_area = max_rectangle(each_bar)
+            if each_area > max_area:
+                max_area = each_area
+        
+        return max_area
